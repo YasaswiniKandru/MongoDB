85434198c5ae093c755865a43a5df933e440803b
Yasaswini Kandru <31980048+YasaswiniKandru@users.noreply.github.com>
2021-04-07 15:24:15 -0400

    Create parts.js

---
+++ parts.js
@@ -0,0 +1,66 @@
+use moDB
+db.parts.deleteMany({})
+
+db.parts.insert({
+  "PNO": 10506,
+  "PNAME": "Land Before Time I",
+  "QOH": 200,
+  "PRICE": 19.99,
+  "OLEVEL": 20
+})
+
+db.parts.insert({
+  "PNO": 10507,
+  "PNAME": "Land Before Time II",
+  "QOH": 156,
+  "PRICE": 19.99,
+  "OLEVEL": 20
+})
+
+db.parts.insert({
+  "PNO": 10508,
+  "PNAME": "Land Before Time III",
+  "QOH": 190,
+  "PRICE": 19.99,
+  "OLEVEL": 20
+})
+
+db.parts.insert({
+  "PNO": 10509,
+  "PNAME": "Land Before Time IV",
+  "QOH": 60,
+  "PRICE": 19.99,
+  "OLEVEL": 20
+})
+
+db.parts.insert({
+  "PNO": 10601,
+  "PNAME": "Sleeping Beauty",
+  "QOH": 300,
+  "PRICE": 24.99,
+  "OLEVEL": 20
+})
+
+db.parts.insert({
+  "PNO": 10701,
+  "PNAME": "When Harry Met Sally",
+  "QOH": 120,
+  "PRICE": 19.99,
+  "OLEVEL": 30
+})
+
+db.parts.insert({
+  "PNO": 10800,
+  "PNAME": "Dirty Harry",
+  "QOH": 140,
+  "PRICE": 14.99,
+  "OLEVEL": 30
+})
+
+db.parts.insert({
+  "PNO": 10900,
+  "PNAME": "Dr. Zhivago",
+  "QOH": 100,
+  "PRICE": 24.99,
+  "OLEVEL": 30
+})
